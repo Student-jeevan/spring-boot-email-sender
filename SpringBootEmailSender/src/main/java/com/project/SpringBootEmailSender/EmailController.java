6add7cbf18e28db349f625af79b8e2f5acb280ea
jeevansingh <jeevanthitrya@gmail.com>
2025-10-05 17:31:08 +0530

    Added HTML email sending feature

--- SpringBootEmailSender/src/main/java/com/project/SpringBootEmailSender/EmailController.java
+++ SpringBootEmailSender/src/main/java/com/project/SpringBootEmailSender/EmailController.java
@@ -1,9 +1,15 @@
 package com.project.SpringBootEmailSender;
 
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 @RestController
 public class EmailController {
@@ -26,5 +32,41 @@ public class EmailController {
             return e.getMessage();
         }
     }
-
+    @RequestMapping("/send-email-with-attach")
+    public String sendEmailWithAttachment(){
+        try{
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper  = new MimeMessageHelper(message , true);
+            helper.setFrom("jeevanthitra@gmail.com");
+            helper.setTo("jeevanthitrya@gmail.com");
+            helper.setSubject("java email with attachments");
+            helper.setText("find email with attachment");
+            helper.addAttachment("Oracle.pdf" , new File("C:\\Users\\hp\\OneDrive\\Desktop\\Certificates\\Oracle.pdf"));
+            helper.addAttachment("semresults.pdf" , new File("C:\\Users\\hp\\OneDrive\\Desktop\\Certificates\\semresults.pdf"));
+            javaMailSender.send(message);
+            return "Success!";
+        }
+        catch(Exception e){
+            return e.getMessage();
+        }
+    }
+    @RequestMapping("/send-html-email")
+    public String sendEmailWithHtmlBody(){
+        try{
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper  = new MimeMessageHelper(message , true);
+            helper.setFrom("jeevanthitra@gmail.com");
+            helper.setTo("jeevanthitrya@gmail.com");
+            helper.setSubject("java email with attachments");
+            helper.setText("find email with attachment");
+            try(var inputStream = Objects.requireNonNull(EmailController.class.getResourceAsStream("/templates/email-sample.html"))){
+                helper.setText(new String(inputStream.readAllBytes() , StandardCharsets.UTF_8), true);
+            }
+            javaMailSender.send(message);
+            return "Success!";
+        }
+        catch(Exception e){
+            return e.getMessage();
+        }
+    }
 }
